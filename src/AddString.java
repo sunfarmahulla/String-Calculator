cad50b2d12bc8f194dbdae3ccaeacdc4badca00a
sunmaker <surajkumars177@gmail.com>
2020-06-06 09:52:03 +0530

    refactor issue 4

--- src/AddString.java
+++ src/AddString.java
@@ -9,10 +9,14 @@ public class AddString {
 			return stringToInt(string);
 		}
 		else {
-			return Integer.parseInt(num[0])+Integer.parseInt(num[1]);
+			return sum(num[0],num[1]);
+			//return Integer.parseInt(num[0])+Integer.parseInt(num[1]);
 		}
 		
 		
+	}
+	private static int sum(String num1, String num2) {
+		return Integer.parseInt(num1)+Integer.parseInt(num2);
 	}
 	
 	private static boolean isEmpty(String string) {
