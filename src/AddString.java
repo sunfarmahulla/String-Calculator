b37f8709079f06a6a959664d103a9c49bf71e5f9
sunmaker <surajkumars177@gmail.com>
2020-06-06 08:26:00 +0530

    Refactor code.first Issue

--- src/AddString.java
+++ src/AddString.java
@@ -1,10 +1,15 @@
 
 public class AddString {
 	public static int addString(String string) {
-		if(string.isEmpty()) {
+		if(isEmpty(string)) {
 			return 0;
 		}
 		return Integer.parseInt(string);
+	}
+
+	private static boolean isEmpty(String string) {
+		
+		return string.isEmpty();
 	}
 
 	
